de65699e8959db66a148a48ca0871505f9611f56
Alex Boury <alexboury@hotmail.fr>
2022-11-13 17:04:59 +0100

    tiles matrix is moving correctly

--- src/main/java/game/classes/KeyBoard.java
+++ src/main/java/game/classes/KeyBoard.java
@@ -1,11 +1,17 @@
 package game.classes;
 
+import javax.swing.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class KeyBoard implements KeyListener {
 
     private KeyMapping mapping;
+    private MapPanel mapPanel;
+
+    public KeyBoard(MapPanel mapPanel){
+        this.mapPanel=mapPanel;
+    }
 
     @Override
     public void keyTyped(KeyEvent e) {
@@ -19,18 +25,22 @@ public class KeyBoard implements KeyListener {
             case KeyMapping.UP:
             case KeyMapping.UP2:
                 System.out.println("UP");
+                mapPanel.moveCameraUp();
                 break;
             case KeyMapping.DOWN:
             case KeyMapping.DOWN2:
                 System.out.println("DOWN");
+                mapPanel.moveCameraDown();
                 break;
             case KeyMapping.LEFT:
             case KeyMapping.LEFT2:
                 System.out.println("LEFT");
+                mapPanel.moveCameraLeft();
                 break;
             case KeyMapping.RIGHT:
             case KeyMapping.RIGHT2:
                 System.out.println("RIGHT");
+                mapPanel.moveCameraRight();
                 break;
         }
     }
--- src/main/java/game/classes/MapPanel.java
+++ src/main/java/game/classes/MapPanel.java
@@ -2,21 +2,40 @@ package game.classes;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.Arrays;
 
 public class MapPanel extends JPanel {
     private final int[] tilesSize = {64, 64};
     private int[][] matrix;
     private final int GRASS = 58;
-
-
+    private int tileDeltaX = 0;
+    private int tileDeltaY = 0;
     public MapPanel(int[][] tilesMap) {
         this.matrix = tilesMap;
-        addKeyListener(new KeyBoard());
+        addKeyListener(new KeyBoard(this));
     }
 
+    public void moveCameraUp(){
+        this.tileDeltaY += 1;
+        repaint();
+    }
+
+    public void moveCameraDown(){
+        this.tileDeltaY -= 1;
+        repaint();
+    }
+
+    public void moveCameraRight(){
+        this.tileDeltaX -= 1;
+        repaint();
+    }
+
+    public void moveCameraLeft(){
+        this.tileDeltaX += 1;
+        repaint();
+    }
 
     public void paint(Graphics graphics) {
+        super.paintComponent(graphics);
         Toolkit toolkit = Toolkit.getDefaultToolkit();
         //this.setLayout(new FlowLayout());
         Image image = toolkit.getImage("C:\\Users\\PC\\IdeaProjects\\AgeOfColonies\\src\\main\\java\\game\\assets\\green.png");
@@ -25,10 +44,9 @@ public class MapPanel extends JPanel {
             for (int col = 0; col < this.matrix[row].length; col++) {
                // System.out.print(this.matrix[row][col] +"\n");
                 if (this.matrix[row][col] == this.GRASS){
-                    graphics.drawImage(image, col*tilesSize[0], row*tilesSize[1], this);
+                    graphics.drawImage(image, col*tilesSize[0] + tilesSize[0] * this.tileDeltaX, row*tilesSize[1] + tilesSize[1] * this.tileDeltaY, this);
                 }
             }
         }
-
     }
 }
